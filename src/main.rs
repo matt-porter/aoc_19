2606b70c4e286ff41a2f7e74a00e869b403b7a78
Matt Porter <matt.porter@cityfibre.com>
2019-12-16 11:32:48 +0000

    Day 3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::fs::File;
 use std::io::{BufRead, BufReader, Read};
 use std::str::FromStr;
 
+#[derive(Debug)]
+struct Move {
+    direction: char,
+    distance: i32,
+}
+
 fn load_input_lines() -> Vec<i32> {
     let f = File::open("input/day1.txt").expect("Failed to open day1.txt");
     BufReader::new(f)
@@ -18,7 +24,87 @@ fn load_input_commas() -> Vec<i32> {
     let mut s = String::new();
     BufReader::new(f)
         .read_to_string(&mut s);
-    split_commas(s)
+    let s= s.trim();
+    split_commas(s.to_owned())
+}
+fn parse_moves(s: String) -> Vec<Move> {
+    let mut moves: Vec<Move> = vec![];
+    for unparsed in s.split(",") {
+        let (direction, distance_part) = unparsed.split_at(1);
+//        println!("distance part: {:?}", &distance_part);
+        let distance = i32::from_str(&distance_part).expect("Failed to parse distance");
+        moves.push(match direction {
+            "U" => Move {direction: 'U', distance},
+            "D" => Move {direction: 'D', distance},
+            "L" => Move {direction: 'L', distance},
+            "R" => Move {direction: 'R', distance},
+            _ => unreachable!()
+        });
+    }
+    moves
+}
+fn load_moves() -> (Vec<Move>, Vec<Move>) {
+    let f = File::open("input/day3.txt").expect("Failed to open day3.txt");
+    let mut lines = BufReader::new(f)
+        .lines();
+    if let Some(Ok(line)) = lines.next() {
+        let move1 = parse_moves(line);
+        if let Some(Ok(line)) = lines.next() {
+            let move2 = parse_moves(line);
+            return (move1, move2)
+        }
+    }
+    unreachable!()
+}
+fn crossovers(wire1: Vec<Move>, wire2: Vec<Move>) -> Vec<(i32, i32, i32)> {
+    let mut path1: Vec<(i32, i32)> = vec![];
+    let mut path2: Vec<(i32, i32)> = vec![];
+    let start = (0,0);
+    let mut current = (0,0);
+    for move_ in wire1 {
+        for distance in 1..=move_.distance {
+            //            println!("Moving 1 {} [{:?}", move_.direction, move_);
+            current = match move_.direction {
+                'U' => (current.0, current.1 + 1),
+                'D' => (current.0, current.1 - 1),
+                'L' => (current.0 - 1, current.1),
+                'R' => (current.0 + 1, current.1),
+                _ => unreachable!()
+            };
+            path1.push(current.clone());
+        }
+    }
+    current = (0,0);
+    for move_ in wire2 {
+        for distance in 1..=move_.distance {
+            //            println!("Moving 1 {} [{:?}", move_.direction, move_);
+            current = match move_.direction {
+                'U' => (current.0, current.1 + 1),
+                'D' => (current.0, current.1 - 1),
+                'L' => (current.0 - 1, current.1),
+                'R' => (current.0 + 1, current.1),
+                _ => unreachable!()
+            };
+            path2.push(current.clone());
+        }
+    }
+//    println!("Path 1 {:?}", &path1);
+//    println!("Path 2 {:?}", &path2);
+    let mut crosses: Vec<(i32, i32, i32)> = Vec::new();
+    for (path1_dist, pair1) in path1.iter().enumerate() {
+        for (path2_dist, pair2) in path2.iter().enumerate() {
+            if pair2 == pair1 {
+                crosses.push((pair1.0, pair1.1, 2 + path1_dist as i32 + path2_dist as i32));
+            }
+        }
+    }
+    crosses
+}
+fn manhatten_distance(pos: &(i32, i32)) -> i32 {
+    pos.0 + pos.1
+}
+fn min_manhatten_distance(crosses: &Vec<(i32, i32, i32)>) -> i32 {
+    crosses.iter().map(|pos| manhatten_distance(&(pos.0, pos.1))).min().expect("No min?")
 }
 
 fn execute(opt: Vec<i32>) -> Vec<i32> {
@@ -81,9 +167,18 @@ fn day2() {
     println!("Day 2, part 1: {:?}", &result[0]);
 }
 
+fn day3() {
+    let (m1, m2) = load_moves();
+    let crosses = crossovers(m1, m2);
+    println!("Found {} crosses", crosses.len());
+    let min_dist = crosses.iter().map(|(x,y,d)| d).min().expect("no min");
+    println!("Day 3: {}", min_dist);
+}
+
 fn main() {
-    day1();
-    day2();
+//    day1();
+//    day2();
+    day3();
 }
 
 #[cfg(test)]
@@ -127,5 +222,42 @@ mod day2 {
     fn test4() {
         let input = split_commas("1,1,1,4,99,5,6,0,99".into());
         assert_eq!(execute(input), split_commas("30,1,1,4,2,5,6,0,99".into()));
+    }
+}
+
+#[cfg(test)]
+mod day3 {
+    use super::*;
+
+    #[test]
+    fn test_0() {
+        let move1 = parse_moves("R8,U5,L5,D3".to_owned());
+        let move2 = parse_moves("U7,R6,D4,L4".to_owned());
+        let crosses = crossovers(move1, move2);
+        assert_eq!(crosses.len(), 2);
+        assert_eq!(min_manhatten_distance(&crosses), 6);
+        let min_dist = crosses.iter().map(|(x,y,d)| d).min().expect("no min");
+        assert_eq!(*min_dist, 30);
+    }
+
+    #[test]
+    fn test_1() {
+        let move1 = parse_moves("R75,D30,R83,U83,L12,D49,R71,U7,L72".to_owned());
+        let move2 = parse_moves("U62,R66,U55,R34,D71,R55,D58,R83".to_owned());
+        let crosses = crossovers(move1, move2);
+        let min_dist = crosses.iter().map(|(x,y,d)| d).min().expect("no min");
+        assert_eq!(*min_dist, 610);
+        assert_eq!(min_manhatten_distance(&crosses), 159);
+
+    }
+
+    #[test]
+    fn test_2() {
+        let move1 = parse_moves("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".to_owned());
+        let move2 = parse_moves("U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".to_owned());
+        let crosses = crossovers(move1, move2);
+        assert_eq!(min_manhatten_distance(&crosses), 135);
+        let min_dist = crosses.iter().map(|(x,y,d)| d).min().expect("no min");
+        assert_eq!(*min_dist, 410);
     }
 }
