4f8f6df28260ad1fc8e7cfc1e226419d30657402
Matt Porter <matt.porter@cityfibre.com>
2019-12-18 08:55:23 +0000

    Day5 (i)

--- src/intcode.rs
+++ src/intcode.rs
@@ -1,16 +1,17 @@
-use crate::input::load_input_commas;
+use crate::input::{load_input_commas, split_commas};
 use std::io::{stdout, Write};
 
 fn input() -> i32 {
-    0
+    1
 }
 
 fn output<W: Write>(val: i32, mut out: &mut W) {
+    println!("out: {}", val);
     out.write(&val.to_ne_bytes());
 }
 
 fn get_value(pos: i32, immediate_mode: bool, data: &Vec<i32>) -> i32{
-    println!("Getting value {} in mode {}", pos, immediate_mode);
+//    println!("Getting value {} in mode {}", pos, immediate_mode);
     if immediate_mode {
         pos
     } else {
@@ -21,11 +22,12 @@ fn get_value(pos: i32, immediate_mode: bool, data: &Vec<i32>) -> i32{
 fn parse_modes(param: i32) -> (bool, bool, bool) {
     let op = param % 100;
     let param = param - op;
-    let m3 = param % 1_000;
-    let param = param - m3;
+    let m1 = param % 1_000;
+    let param = param - m1;
     let m2 = param % 10_000;
     let param = param - m2;
-    (param == 10_000, m2 == 1_000, m3 == 100)
+//    println!("Parsed as {} {} {} {}", m1, m2, param, op);
+    (m1 == 100, m2 == 1_000, param == 10_000)
 }
 
 fn execute<W: Write>(opt: Vec<i32>, mut out: &mut W) -> Vec<i32> {
@@ -38,19 +40,36 @@ fn execute<W: Write>(opt: Vec<i32>, mut out: &mut W) -> Vec<i32> {
         let op = data[iptr] % 100;
         let (m1, m2, m3) = parse_modes(data[iptr]);
 //        println!("{}, {}, {}, {}", op, data[iptr+1], data[iptr+2], data[iptr+3]);
-        let v1 = data[iptr+1];
-        let v2 = data[iptr+2];
-        let v3 = data[iptr+3];
+        let v1 = *data.get(iptr+1).unwrap_or(&0);
+        let v2 = *data.get(iptr+2).unwrap_or(&0);
+        let v3 = *data.get(iptr+3).unwrap_or(&0);
+//        println!("Op {}", op);
         match op {
-            1 => data[v3 as usize] = get_value(v1, m1, &data) + get_value(v2, m2, &data),
-            2 => data[v3 as usize] = data[v1 as usize] * data[v2 as usize],
-            3 => data[v1 as usize] = input(),
-            4 => output(data[v1 as usize], out),
+            1 => {
+//                println!("data[{}] = get({}, {}) + get({}, {})", v3, v1, m1, v2, m2);
+                data[v3 as usize] = get_value(v1, m1, &data) + get_value(v2, m2, &data);
+                iptr += 4;
+            },
+            2 => {
+                data[v3 as usize] = get_value(v1, m1, &data) * get_value(v2, m2, &data);
+                iptr += 4;
+            },
+            3 => {
+                data[v1 as usize] = input();
+                iptr += 2;
+            },
+            4 => {
+                let val = get_value(v1, m1, &data);
+                if val != 0 {
+                    println!("Failed at {}, previous instructions were: {:?} {:?}", iptr, &data[iptr-8..iptr], &data[iptr..iptr+4])
+                }
+                output(get_value(v1, m1, &data), out);
+                iptr += 2;
+
+            },
             99 => break,
-            _ => unimplemented!()
+            _ => unimplemented!("No opcode {}", op)
         }
-//        println!("{:?}", &data);
-        iptr += 4;
     }
     data
 }
@@ -68,7 +87,6 @@ pub fn day2() {
             if result == 19_690_720 {
                 println!("Day 2, part 2: {}", 100 * i + j);
             }
-
         }
     }
     ops[1] = 12;
@@ -78,7 +96,11 @@ pub fn day2() {
 }
 
 pub fn day5() {
-
+    let mut writer: Vec<u8> = Vec::new();
+    let input_s = "3,225,1,225,6,6,1100,1,238,225,104,0,1101,69,55,225,1001,144,76,224,101,-139,224,224,4,224,1002,223,8,223,1001,224,3,224,1,223,224,223,1102,60,49,225,1102,51,78,225,1101,82,33,224,1001,224,-115,224,4,224,1002,223,8,223,1001,224,3,224,1,224,223,223,1102,69,5,225,2,39,13,224,1001,224,-4140,224,4,224,102,8,223,223,101,2,224,224,1,224,223,223,101,42,44,224,101,-120,224,224,4,224,102,8,223,223,101,3,224,224,1,223,224,223,1102,68,49,224,101,-3332,224,224,4,224,1002,223,8,223,1001,224,4,224,1,224,223,223,1101,50,27,225,1102,5,63,225,1002,139,75,224,1001,224,-3750,224,4,224,1002,223,8,223,1001,224,3,224,1,223,224,223,102,79,213,224,1001,224,-2844,224,4,224,102,8,223,223,1001,224,4,224,1,223,224,223,1,217,69,224,1001,224,-95,224,4,224,102,8,223,223,1001,224,5,224,1,223,224,223,1102,36,37,225,1101,26,16,225,4,223,99,0,0,0,677,0,0,0,0,0,0,0,0,0,0,0,1105,0,99999,1105,227,247,1105,1,99999,1005,227,99999,1005,0,256,1105,1,99999,1106,227,99999,1106,0,265,1105,1,99999,1006,0,99999,1006,227,274,1105,1,99999,1105,1,280,1105,1,99999,1,225,225,225,1101,294,0,0,105,1,0,1105,1,99999,1106,0,300,1105,1,99999,1,225,225,225,1101,314,0,0,106,0,0,1105,1,99999,1107,677,677,224,102,2,223,223,1006,224,329,1001,223,1,223,1108,677,677,224,1002,223,2,223,1006,224,344,1001,223,1,223,107,226,226,224,1002,223,2,223,1006,224,359,101,1,223,223,1008,226,226,224,102,2,223,223,1005,224,374,1001,223,1,223,1107,226,677,224,1002,223,2,223,1006,224,389,1001,223,1,223,1008,677,226,224,1002,223,2,223,1005,224,404,1001,223,1,223,7,677,226,224,102,2,223,223,1005,224,419,1001,223,1,223,1008,677,677,224,1002,223,2,223,1006,224,434,1001,223,1,223,108,226,226,224,102,2,223,223,1006,224,449,1001,223,1,223,108,677,677,224,102,2,223,223,1006,224,464,1001,223,1,223,107,226,677,224,1002,223,2,223,1005,224,479,101,1,223,223,1108,226,677,224,1002,223,2,223,1006,224,494,1001,223,1,223,107,677,677,224,1002,223,2,223,1006,224,509,101,1,223,223,7,677,677,224,102,2,223,223,1006,224,524,1001,223,1,223,1007,226,677,224,1002,223,2,223,1005,224,539,1001,223,1,223,8,226,677,224,1002,223,2,223,1005,224,554,101,1,223,223,8,677,677,224,102,2,223,223,1005,224,569,101,1,223,223,7,226,677,224,102,2,223,223,1006,224,584,1001,223,1,223,1007,226,226,224,102,2,223,223,1006,224,599,1001,223,1,223,1107,677,226,224,1002,223,2,223,1006,224,614,1001,223,1,223,1108,677,226,224,1002,223,2,223,1005,224,629,1001,223,1,223,1007,677,677,224,102,2,223,223,1006,224,644,1001,223,1,223,108,226,677,224,102,2,223,223,1005,224,659,101,1,223,223,8,677,226,224,1002,223,2,223,1006,224,674,1001,223,1,223,4,223,99,226";
+    let input = split_commas(input_s.into());
+    let data_out = execute(input, &mut writer);
+    println!("{:?}", writer);
 }
 
 #[cfg(test)]
@@ -87,7 +109,7 @@ mod day5 {
     use crate::input::split_commas;
 
     #[test]
-    fn test_eg1() {
+    fn test_input() {
         let mut writer: Vec<u8> = Vec::new();
         let input = split_commas("3,0,4,0,99".into());
         assert_eq!(execute(input, &mut writer), split_commas("3,0,4,0,99".into()));
@@ -107,7 +129,8 @@ mod day5 {
         assert_eq!(parse_modes(1002), (false,true,false));
         assert_eq!(parse_modes(11102), (true,true,true));
         assert_eq!(parse_modes(2), (false,false,false));
-        assert_eq!(parse_modes(11002), (true,true,false));
+        assert_eq!(parse_modes(11002), (false,true,true));
+        assert_eq!(parse_modes(1101), (true,true,false));
 
     }
 
